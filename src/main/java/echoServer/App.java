90e4c176f6894896173209a4d4bb9d27cfa3e80d
junehoe <br1ansun9@gmail.com>
2019-11-13 10:13:51 -0800

    Creates tests for system input/output

--- src/main/java/echoServer/App.java
+++ src/main/java/echoServer/App.java
@@ -3,9 +3,22 @@
  */
 package echoServer;
 
+import java.util.Scanner;
+
 public class App {
     public String getGreeting() {
         return "Hello world.";
+    }
+
+    public void printHelloWorld() {
+        System.out.println("Hello World!");
+    }
+
+    public String getInput() {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        scan.close();
+        return s;
     }
 
     public static void main(String[] args) {
--- src/test/java/echoServer/AppTest.java
+++ src/test/java/echoServer/AppTest.java
@@ -4,11 +4,50 @@
 package echoServer;
 
 import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
 public class AppTest {
+    App classUnderTest;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final InputStream originalIn = System.in;
+    private final PrintStream originalOut = System.out;
+
+    @Before public void initialize() {
+        classUnderTest = new App();
+    }
+
+    @Before public void setupStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After public void restoreStreams() {
+        System.setIn(originalIn);
+        System.setOut(originalOut);
+    }
+
     @Test public void testAppHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    }
+
+    @Test public void testHelloWorldOutput() {
+        classUnderTest.printHelloWorld();
+
+        assertEquals("Hello World!\n", outContent.toString());
+    }
+
+    @Test public void testSystemInput() {
+        String inputStr = "Hello!";
+        InputStream input = new ByteArrayInputStream(inputStr.getBytes());
+        System.setIn(input);
+
+        assertEquals("Hello!", classUnderTest.getInput());
     }
 }
