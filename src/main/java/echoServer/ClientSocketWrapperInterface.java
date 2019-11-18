99f4639e9c69c66573d1a1966860d46b2f79c7bc
junehoe <br1ansun9@gmail.com>
2019-11-18 12:37:16 -0800

    Removes the ClientSocket

--- src/main/java/echoServer/ClientSocketWrapperInterface.java
+++
@@ -1,7 +0,0 @@
-package echoserver;
-public interface ClientSocketWrapperInterface {
-    void createSocket(String ip, int port);
-    String receiveData();
-    void sendData(String data);
-    void close();
-}
