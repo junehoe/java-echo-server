abb1f50fa8706cddd7fe5f147dbd73b2c8bdb84a
junehoe <br1ansun9@gmail.com>
2019-11-20 11:43:43 -0800

    Adds logic to start echo server inside App class

--- src/main/java/echoServer/App.java
+++ src/main/java/echoServer/App.java
@@ -1,26 +1,29 @@
 package echoserver;
 
-import java.util.Scanner;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.IOException;
+import java.lang.Thread;
 
 public class App {
-
-    public String getGreeting() {
-        return "Hello world.";
-    }
-
-    public void printHelloWorld() {
-        System.out.println("Hello World!");
-    }
-
-    public String getInput() {
-        Scanner scan = new Scanner(System.in);
-        String s = scan.next();
-        scan.close();
-        return s;
-    }
-
     public static void main(String[] args) {
-        Server server = new Server(new ServerSocketWrapper());
-        server.start(4242);
+        if (args.length != 1) {
+            System.err.println("Port number was not specified");
+            System.exit(1);
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        try {
+            ServerSocket serverSocket = SocketCreator.createServerSocket(port);
+            System.out.println("Server started on port " + port);
+            while (true) {
+                Socket clientSocket = SocketCreator.createClientSocket(serverSocket);
+                Thread echoThread = new Thread(new EchoServer(clientSocket));
+                echoThread.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
