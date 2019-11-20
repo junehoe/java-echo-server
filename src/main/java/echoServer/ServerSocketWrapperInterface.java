a6c6ad29e5c3c594ee13099b5ea51fb1b8cb0f40
junehoe <br1ansun9@gmail.com>
2019-11-20 11:52:53 -0800

    Removes deleted files

--- src/main/java/echoServer/ServerSocketWrapperInterface.java
+++
@@ -1,8 +0,0 @@
-package echoserver;
-
-public interface ServerSocketWrapperInterface {
-    void createSocket(int port);
-    String receiveData();
-    void sendData(String data);
-    void close();
-}
